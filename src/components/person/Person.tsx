2ed4e4b10f2bbaab731bc178e28c8088a8c36821
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2021-04-23 00:41:59 +0000

    Update Person.tsx

--- src/components/person/Person.tsx
+++ src/components/person/Person.tsx
@@ -10,6 +10,16 @@ export function Person({ person }: Props): JSX.Element {
   return (
     <div className={s.person}>
       <h1>{person.name}</h1>
+      <h2>Birth year:</h2>
+      <p>{person.birthYear}</p>
+      <h2>Eye color:</h2>
+      <p>{person.eyeColor}</p>
+      <h2>Hair color:</h2>
+      <p>{person.hairColor}</p>
+      <h2>Height:</h2>
+      <p>{person.height} cm</p>
+      <h2>Mass:</h2>
+      <p>{person.mass} kg</p>
       <Link href="/characters">Back to characters</Link>
     </div>
   );
