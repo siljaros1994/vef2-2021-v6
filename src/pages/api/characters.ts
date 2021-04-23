599872ecdfd4f80cac53da5cda7417edc357644b
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2021-04-23 00:42:08 +0000

    Update characters.ts

--- src/pages/api/characters.ts
+++ src/pages/api/characters.ts
@@ -1,8 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-
+import {fetchCharacters } from '../../lib/swapi';
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-  const after = req.query?.after as string | null;
+  const after = req.query?.after as string | '';
   // TODO sækja næstu síðu af gögnum hér
-  res.status(200).json(null);
+  const characters = await fetchCharacters(after);
+  res.status(200).json(characters);
 };
